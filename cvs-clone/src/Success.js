f926aa76724ffd5fe1d473fd6cdb70ed50ee982d
jerome2l <55860345+J3r0m3L@users.noreply.github.com>
2021-04-17 22:18:22 -0700

    Deployed on Firebase just cuz

--- cvs-clone/src/Success.js
+++ cvs-clone/src/Success.js
@@ -9,7 +9,7 @@ function Success() {
         <h3 id="visit_type"><b>Visit Type: </b>PAT MODERNA COV19 VACCINE 2ND DOSE</h3>
         <h3 id="date"><b>Date and Time: </b>5/8/2021 at 9:00AM PDT</h3>
         <h3 id="location"><b>Location Address: </b>9730 Hopkins Dr, La Jolla, 92093</h3>
-        <h3 id="csn">CSN: 66089117984</h3>
+        <h3 id="csn">CSN: 123456789</h3>
         <h3>Please bring email! Patients under 18 years old must be accompanied by guardian. After the vaccine you
         will be asked to remain on location for a 15-minute observation period. Please bring confirmation email
         and photo ID</h3>
